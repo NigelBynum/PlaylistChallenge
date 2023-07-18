38c45175eda0b280b31bb9c7bbbe92429cf4ade5
nigel <bynum1001@gmail.com>
2023-07-18 14:05:50 -0400

    Done

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,14 +1,35 @@
 package io.zipcoder;
 
 public class Music {
-
     private String[] playList;
-
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
+    public Integer selection(Integer startIndex, String selection) {
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+        int i = startIndex;
+        int j = startIndex;
+        int counterUp = 0;
+        int counterDown = 0;
+
+        while (!playList[i].equals(selection)) {
+            if (i == playList.length - 1) {
+                i = 0;}
+            else {i++;}
+
+            counterUp++;
+        }
+        while (!playList[j].equals(selection)) {
+            if (j == 0) {
+                j = playList.length - 1;}
+            else {j--;}
+
+            counterDown++;
+        }
+        if (counterDown < counterUp) {
+
+            return counterDown;
+        }
+        return counterUp;
     }
 }
